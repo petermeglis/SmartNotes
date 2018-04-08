d9f0e470c7196718363b9318f90a07375a2e0644
Peter Meglis <petermeglis@gmail.com>
2018-04-08 03:09:50 -0400

    Added audio recording capability to main screen

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import MainPage from './MainPage';
 
-AppRegistry.registerComponent('SmartNotes', () => App);
+AppRegistry.registerComponent('SmartNotes', () => MainPage);
